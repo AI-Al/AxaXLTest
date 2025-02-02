54cc7ee75437887ef746430acc13cba162b0d784
Alex W <mavrm@hotmail.com>
2025-02-02 18:12:29 +0000

    Add tests for updating summaries

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -63,3 +63,82 @@ def test_remove_summary_incorrect_id(test_app_with_db):
     response = test_app_with_db.delete("/summaries/999")
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
+
+def test_update_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}", 
+        data=json.dumps({"url": "https://foo.bar", "summary": "new summary"})
+    )
+    assert response.status_code == 200
+    response_dict = response.json()
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["created_at"] 
+    assert response_dict["summary"] == "new summary"
+
+def test_update_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.put(
+        "/summaries/999", 
+        data=json.dumps({"url": "https://foo.bar", "summary": "new summary"})
+    )
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+def test_update_summary_invalid_json(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}",
+        data=json.dumps({}),
+    )
+
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "input": {},
+                "loc": ["body", "url"],
+                "msg": "Field required",
+                "type": "missing",
+                "url": "https:errors.pydantic.dev/2.5/v/missing",
+            },
+            {
+                "input": {},
+                "loc": ["body", "summary"],
+                "msg": "Field required",
+                "type": "missing",
+                "url": "https:errors.pydantic.dev/2.5/v/missing",
+            }
+        ]
+    }
+
+def test_update_summary_invalid_keys(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}",
+        data=json.dumps({"url": "https://foo.bar"})
+    )
+
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "input": {"url": "https://foo.bar"},
+                "loc": ["body", "summary"],
+                "msg": "Field required",
+                "type": "missing",
+                "url": "https:errors.pydantic.dev/2.5/v/missing",
+            }
+        ]
+    }
