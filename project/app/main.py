06f6556b6e78285b43b6b9ddca42408ae8fb02b5
Alex W <mavrm@hotmail.com>
2025-02-02 14:18:01 +0000

    refactor of startup and shutdown

--- project/app/main.py
+++ project/app/main.py
@@ -1,23 +1,32 @@
-import os
-from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
-from app.api import ping
+import logging
 
-# from app.models.tortoise import TextSummary
+from fastapi import FastAPI
+
+from app.api import ping
+from app.db import init_db
+from contextlib import asynccontextmanager
+
+
+
+log = logging.getLogger("uvicorn")
 
 def create_application() -> FastAPI:
     application = FastAPI()
-
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=False,
-        add_exception_handlers=True,
-    )
-
     application.include_router(ping.router)
+
     return application
 
 app = create_application()
 
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Start up
+    log.info("Starting up...")
+    init_db(app)
+    
+    yield
+    # Shut down
+    log.info("Shutting down...")
+    
