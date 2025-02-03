4b98855711c0c324e542906907ca1050408b6574
Alex W <mavrm@hotmail.com>
2025-02-03 21:43:22 +0000

    tidyup

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -1,5 +1,4 @@
 import json
-
 import pytest
 
 
@@ -10,6 +9,7 @@ def test_create_summary(test_app_with_db):
 
     assert response.status_code == 201
     assert response.json()["url"] == "https://foo.bar/"
+
 
 def test_create_summaries_invalid_json(test_app_with_db):
     response = test_app_with_db.post("/summaries/", data=json.dumps({}))
@@ -25,10 +25,16 @@ def test_create_summaries_invalid_json(test_app_with_db):
         ]
     }
 
+
 def test_create_summaries_invalid_json_2(test_app_with_db):
-    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "invalid://url"}))
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "invalid://url"})
+    )
     assert response.status_code == 422
-    assert response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
+    assert (
+        response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
+    )
+
 
 def test_read_summary(test_app_with_db):
     response = test_app_with_db.post(
@@ -57,10 +63,10 @@ def test_read_summary_incorrect_id(test_app_with_db):
         "detail": [
             {
                 "ctx": {"gt": 0},
-                "input" : "0",
+                "input": "0",
                 "loc": ["path", "id"],
                 "msg": "Input should be greater than 0",
-                "type": "greater_than",                
+                "type": "greater_than",
             }
         ]
     }
@@ -91,6 +97,7 @@ def test_remove_summary(test_app_with_db):
     response = test_app_with_db.get(f"/summaries/{summary_id}")
     assert response.status_code == 404
 
+
 def test_remove_summary_incorrect_id(test_app_with_db):
     response = test_app_with_db.delete("/summaries/999/")
     assert response.status_code == 404
@@ -102,7 +109,7 @@ def test_remove_summary_incorrect_id(test_app_with_db):
         "detail": [
             {
                 "ctx": {"gt": 0},
-                "input" : "0",
+                "input": "0",
                 "loc": ["path", "id"],
                 "msg": "Input should be greater than 0",
                 "type": "greater_than",
@@ -117,41 +124,44 @@ def test_update_summary(test_app_with_db):
     )
     summary_id = response.json()["id"]
     response = test_app_with_db.put(
-        f"/summaries/{summary_id}", 
-        data=json.dumps({"url": "https://foo.bar", "summary": "new summary"})
+        f"/summaries/{summary_id}",
+        data=json.dumps({"url": "https://foo.bar", "summary": "new summary"}),
     )
     assert response.status_code == 200
     response_dict = response.json()
     assert response_dict["id"] == summary_id
     assert response_dict["url"] == "https://foo.bar/"
-    assert response_dict["created_at"] 
+    assert response_dict["created_at"]
     assert response_dict["summary"] == "new summary"
+
 
 def test_update_summary_incorrect_id(test_app_with_db):
     response = test_app_with_db.put(
-        "/summaries/999", 
-        data=json.dumps({"url": "https://foo.bar", "summary": "new summary"})
+        "/summaries/999",
+        data=json.dumps({"url": "https://foo.bar", "summary": "new summary"}),
     )
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
 
+
 def test_update_summary_incorrect_id_2(test_app_with_db):
     response = test_app_with_db.put(
-        "/summaries/0", 
-        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+        "/summaries/0",
+        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
     )
     assert response.status_code == 422
     assert response.json() == {
         "detail": [
             {
                 "ctx": {"gt": 0},
-                "input" : "0",
+                "input": "0",
                 "loc": ["path", "id"],
                 "msg": "Input should be greater than 0",
-                "type": "greater_than",                
+                "type": "greater_than",
             }
         ]
     }
+
 
 def test_update_summary_invalid_json(test_app_with_db):
     response = test_app_with_db.post(
@@ -178,9 +188,10 @@ def test_update_summary_invalid_json(test_app_with_db):
                 "loc": ["body", "summary"],
                 "msg": "Field required",
                 "type": "missing",
-            }
+            },
         ]
     }
+
 
 def test_update_summary_invalid_keys(test_app_with_db):
     response = test_app_with_db.post(
@@ -189,8 +200,7 @@ def test_update_summary_invalid_keys(test_app_with_db):
     summary_id = response.json()["id"]
 
     response = test_app_with_db.put(
-        f"/summaries/{summary_id}",
-        data=json.dumps({"url": "https://foo.bar"})
+        f"/summaries/{summary_id}", data=json.dumps({"url": "https://foo.bar"})
     )
 
     assert response.status_code == 422
@@ -207,7 +217,9 @@ def test_update_summary_invalid_keys(test_app_with_db):
 
     response = test_app_with_db.put(
         f"/summaries/{summary_id}/",
-        data=json.dumps({"url": "invalid://url", "summary": "updated!"})
+        data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
     )
     assert response.status_code == 422
-    assert response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
+    assert (
+        response.json()["detail"][0]["msg"] == "URL scheme should be 'http' or 'https'"
+    )
